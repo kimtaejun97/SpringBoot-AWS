44d7b7d53dbf6268540c1514a460af1fe915bd71
kimtaejun <kim_taejun@naver.com>
2021-06-19 15:40:12 +0900

    무중단 배포 스크립트

---
+++ src/test/java/com/study/springboot/web/ProfileControllerUnitTest.java
@@ -0,0 +1,25 @@
+package com.study.springboot.web;
+
+import org.junit.Test;
+import org.springframework.mock.env.MockEnvironment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ProfileControllerUnitTest {
+
+    @Test
+    public  void real_profile_조회(){
+        String expectedProfile = "real";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("oauth");
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        String profile = controller.profile();
+
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+}
